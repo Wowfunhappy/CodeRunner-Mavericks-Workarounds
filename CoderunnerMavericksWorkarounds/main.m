61a75bc1203805caf8ce5d43cd0554667e91bcfe
Jonathan Alland <jonathan@Jonathans-Mac.local>
2021-03-31 14:11:40 -0400

    mend

---
+++ CoderunnerMavericksWorkarounds/main.m
@@ -0,0 +1,22 @@
+#import <Foundation/Foundation.h>
+#import "ZKSwizzle.h"
+
+@interface myNSMethodSignature : NSObject
+@end
+
+@implementation myNSMethodSignature
++ (id)signatureWithObjCTypes:(const char *)types {
+    if (strcmp(types, "") == 0) {
+        return nil;
+    }
+    return ZKOrig(id, types);
+}
+@end
+
+@implementation NSObject (main)
+
++ (void)load {
+    ZKSwizzle(myNSMethodSignature, NSMethodSignature);
+}
+
+@end
