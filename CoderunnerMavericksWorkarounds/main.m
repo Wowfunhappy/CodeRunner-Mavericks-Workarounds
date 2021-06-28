b9e8c8d3d879986ce6b3edca23c501b3382853cd
Jonathan Alland <jonathan@Mavericks-VM.local>
2021-06-28 13:06:19 -0400

    Fixed #2

--- CoderunnerMavericksWorkarounds/main.m
+++ CoderunnerMavericksWorkarounds/main.m
@@ -1,8 +1,13 @@
 #import <Foundation/Foundation.h>
+#import <AppKit/AppKit.h>
 #import "ZKSwizzle.h"
+
+
+
 
 @interface myNSMethodSignature : NSObject
 @end
+
 
 @implementation myNSMethodSignature
 + (id)signatureWithObjCTypes:(const char *)types {
@@ -13,8 +18,27 @@
 }
 @end
 
+
+
+
+@interface myNSFindIndicatorOverlayView : NSView
+@end
+
+
+@implementation myNSFindIndicatorOverlayView
+
+- (void)drawRect:(struct CGRect)arg1 {
+    //Do nothing. Avoids bug when switching tabs while text is highlighted.
+}
+
+@end
+
+
+
+
 @implementation NSObject (main)
 + (void)load {
     ZKSwizzle(myNSMethodSignature, NSMethodSignature);
+    ZKSwizzle(myNSFindIndicatorOverlayView, _NSFindIndicatorOverlayView);
 }
 @end
