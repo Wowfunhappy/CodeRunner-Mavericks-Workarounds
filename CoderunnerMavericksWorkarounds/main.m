4c59faccc61952a5954fa35704cf4dcde1eabf10
Jonathan Alland <jonathan@Mavericks-VM.local>
2024-01-01 19:54:04 -0500

    If everything doesn't work now so help me god...

--- CoderunnerMavericksWorkarounds/main.m
+++ CoderunnerMavericksWorkarounds/main.m
@@ -3,6 +3,8 @@
 #import "ZKSwizzle.h"
 
 
+
+
 @interface myNSTabView : NSTabView
 @end
 
@@ -54,9 +56,8 @@
 
 /* Fixes most crashes, such as when the user opens Preferences.
  
- I don't know why this works. I'm a simple guy. I look at the crash log, and the crash log says the app
- crashed because signatureWithObjCTypes's type signature was empty. So I made it so it can't be empty.
- And now the app doesn't crash. */
+ The crash log said the app crashed because signatureWithObjCTypes's type signature was empty.
+ So I made it so it can't be empty. And now the app doesn't crash. Okay. */
 
 @interface myNSMethodSignature : NSObject
 @end
@@ -105,26 +106,6 @@ BOOL shouldPreventNSAttributeDictionaryRelease; //Warning: global variable!
 @implementation myEditor
 
 /*
- CodeRunner crashes when the user tries to print (As in, to a paper printer.)
- (I don't know who prints out their code, but accidental keypresses happen and CodeRunner should never crash!)
- 
- The crash is caused by a use-after-free error. We can fix it crudely by no-op'ing NSAttributeDictionary's release
- method, but let's do so as infrequently as possible. Leaking a few bytes of memory only when the app would have
- otherwised crashed > leaking all the memory all the time.
- 
- (This solution still bothers me, but hey, using Big Sur and/or Electron would waste far more memory...)
- 
- Todo: Fix this...?
- */
-
-- (void)print:(id)arg1 {
-    NSLog(@"CodeRunnerMavericksWorkarounds: Intentionally forcing CodeRunner to leak memory to avert a crash.");
-    shouldPreventNSAttributeDictionaryRelease = true;
-    ZKOrig(void, arg1);
-    shouldPreventNSAttributeDictionaryRelease = false;
-}
-
-/*
  Highlight matching parenthesis/brackets (for more than a few seconds).
  Not technically a bug fix, but a feature I need!
  */
@@ -132,7 +113,7 @@ BOOL shouldPreventNSAttributeDictionaryRelease; //Warning: global variable!
 - (void)showFindIndicatorForRange:(NSRange)charRange {
     ZKOrig(void, charRange);
     [[self textStorage] addAttribute:NSBackgroundColorAttributeName
-                               value:[NSColor colorWithCalibratedWhite:0.5 alpha:0.25]
+                               value:[NSColor colorWithCalibratedWhite:0.5 alpha:0.5]
                                range:charRange];
     
     NSValue *rangeValue = [NSValue valueWithRange:charRange];
@@ -151,6 +132,20 @@ BOOL shouldPreventNSAttributeDictionaryRelease; //Warning: global variable!
     [super quickLookWithEvent:arg1];
 }
 
+/*
+ CodeRunner crashes when the user tries to print (As in, to a paper printer.)
+ 
+ The crash is caused by a use-after-free error. We can fix it crudely by temporarily no-op'ing NSAttributeDictionary's release
+ method. This is kind of ugly, but the amount of memory leaked is exceedingly trivial.
+ */
+
+- (void)print:(id)arg1 {
+    NSLog(@"CodeRunnerMavericksWorkarounds: Intentionally forcing CodeRunner to leak memory to avert a crash.");
+    shouldPreventNSAttributeDictionaryRelease = true;
+    ZKOrig(void, arg1);
+    shouldPreventNSAttributeDictionaryRelease = false;
+}
+
 @end
 
 
@@ -164,7 +159,7 @@ BOOL shouldPreventNSAttributeDictionaryRelease; //Warning: global variable!
 
 - (oneway void)release {
     if (shouldPreventNSAttributeDictionaryRelease) {
-        //Leak the memory instead of freeing it!
+        //See above; leak the memory instead of freeing it.
         return;
     }
     ZKOrig(void);
@@ -212,7 +207,7 @@ BOOL shouldPreventNSAttributeDictionaryRelease; //Warning: global variable!
 
 @implementation myConsoleTextView
 
-//Fixes: In a console which outputs lots of text quickly, it's very difficult to scroll up!
+//Without this fix, it's difficult to scroll up in a console which outputs lots of text quickly.
 //I'm not sure if this is Mavericks-specific, but it's annoying and worth fixing.
 
 - (BOOL)shouldScrollToBottom {
@@ -231,204 +226,82 @@ BOOL shouldPreventNSAttributeDictionaryRelease; //Warning: global variable!
 
 
 
-@interface myProcess : NSObject
+@interface Process : NSObject
 @end
-
-
-@implementation myProcess
-
-- (void)startProcessWithExecutablePath:(id)arg1 args:(id)arg2 env:(id)arg3 width:(int)arg4 height:(int)arg5 encoding:(unsigned long long)arg6 {
-    NSLog(@"(void)startProcessWithExecutablePath:(id)%@ args:(id)%@ env:(id)%@ width:(int)%d height:(int)%d encoding:(unsigned long long)%llu", arg1, arg2, arg3, arg4, arg5, arg6);
-    ZKOrig(void, arg1, arg2, arg3, arg4, arg5, arg6);
-    
-    /*NSTask *task = [[NSTask alloc] init];
-     
-     // Setting launch path to 1st parameter 'arg1'
-     task.launchPath = arg1;
-     
-     // Setting arguments to 2nd parameter 'arg2'
-     task.arguments = arg2;
-     
-     // Setting environment to 3rd parameter 'arg3'
-     task.environment = arg3;
-     
-     @try {
-     [task launch];
-     } // Catches and logs if there happens to be any errors
-     @catch (NSException *exception) {
-     NSLog(@"Failed to start task: %@", [exception description]);
-     }*/
-    
-    
-}
-
-- (void)waitForExitStatus {
-    NSLog(@"(void)waitForExitStatus");
-    
-    //    int fileDescriptor = ZKHookIvar(self, int, "fileDescriptor");
-    //    close(fileDescriptor);
-    
-    ZKOrig(void);
-}
-
-- (void)readWriteError {
-    NSLog(@"(void)readWriteError");
-    ZKOrig(void);
-}
-
-- (void)read {
-    //NSLog(@"(void)read");
-    ZKOrig(void);
-}
-
-- (void)write {
-    //NSLog(@"(void)write");
-    ZKOrig(void);
-}
-
-- (int)getFileDescriptor {
-    return ZKHookIvar(self, int, "fileDescriptor");
-}
-
-@end
-
-
-
 
 @interface myProcessManager : NSObject
-{
-    BOOL shouldTerminate; //this is bad, deal with later.
-}
 @end
 
 @implementation myProcessManager
 
+/*
+An intermittent bug which has been driving me crazy for years: at some point after you've been using
+CodeRunner for a while (running and editing different code and on), you'll press the run button but
+nothing will appear in the console window, and CodeRunner will begin consuming a huge amount of CPU.
+You can cancel and re-run the script, which might work that time, but the problem will become
+increasingly frequent until you restart CodeRunner.
+
+The problem has something to do with this mainLoop method. This is my third attempt to fix this bug,
+so I'm going to just reimplement the whole method with a super simple version. The original (I think)
+used file descriptors to only read when there was new data available, but we're just going to read after
+a fixed time interval. And if I encounter the bug again after all of this, I am going to scream...
+*/
+
 -(void)mainLoop {
-    //self->shouldTerminate = NO;
-    
-    while (true /*!self->shouldTerminate*/) {
-        for (myProcess *process in [self getProcesses]) {
-            [process read];
-            if ([process shouldWrite]) {
-                [process write];
-            }
-        }
-        [NSThread sleepForTimeInterval:0.01];
-        
-        /*[[self getLock] lock];
-        
-        for (myProcess *process in [self getProcesses]) {
-            int fd = [process getFileDescriptor];
-            if (fd < 0) {
-                [self removeProcess:process];
-                NSLog(@"process removed!");
-            } else {
+    while (![[NSThread currentThread] isCancelled]) {
+        while ([[self getProcesses] count] > 0) {
+            for (Process *process in [self getProcesses]) {
                 [process read];
                 if ([process shouldWrite]) {
                     [process write];
                 }
             }
+            [NSThread sleepForTimeInterval:0.005];
         }
-        [[self getLock] unlock];*/
+        [NSThread sleepForTimeInterval:0.1];
     }
-}
-
-- (void)shouldTerminateLoop {
-    self->shouldTerminate = YES;
-}
-
-- (NSRecursiveLock*)getLock {
-    return ZKHookIvar(self, NSRecursiveLock *, "lock");
 }
 
 - (NSMutableArray*)getProcesses {
     return ZKHookIvar(self, NSMutableArray *, "processes");
 }
 
-- (void)addProcess:(id)arg1 {
-    NSLog(@"(void)addProcess:(id)%@", arg1);
-    ZKOrig(void, arg1);
-}
-
-- (void)removeProcess:(id)arg1 {
-    NSLog(@"(void)removeProcess:(id)%@", arg1);
-    ZKOrig(void, arg1);
-}
-
-- (void)resume {
-    NSLog(@"(void)resume");
-    ZKOrig(void);
-}
-
-@end
-
-
-
-@interface myRunner : NSObject
-@end
-
-
-@implementation myRunner
-
-- (void)process:(id)arg1 didReadData:(id)arg2 {
-    //NSLog(@"(void)process:(id)%@ didReadData:(id)%@", arg1, arg2);
-    ZKOrig(void, arg1, arg2);
-}
-
-- (void)process:(id)arg1 didExitWithStatus:(int)arg2 time:(float)arg3; {
-    NSLog(@"(void)process:(id)%@ didExitWithStatus:(int)%d time:(float)%f", arg1, arg2, arg3);
-    ZKOrig(void, arg1, arg2, arg3);
-}
-
-- (void)processDidExit:(id)arg1 {
-    NSLog(@"(void)processDidExit:(id)%@", arg1);
-    ZKOrig(void, arg1);
-}
-
-- (void)runWithFilePath:(id)arg1 {
-    NSLog(@"(void)runWithFilePath:(id)%@", arg1);
-    ZKOrig(void, arg1);
-}
-
-- (void)run:(id)arg1 {
-    NSLog(@"(void)run:(id)%@", arg1);
-    ZKOrig(void, arg1);
-}
-
-- (void)stopRunningWithStatus:(int)arg1 {
-    NSLog(@"(void)stopRunningWithStatus:(int)%d", arg1);
-    ZKOrig(void, arg1);
-}
-
 @end
 
 
 
 
-@interface myNSThread : NSThread
+@interface myDocument : NSDocument
+{
+    NSView *documentView;
+}
 @end
 
 
-@implementation myNSThread
+@implementation myDocument
 
-+ (void)detachNewThreadSelector:(SEL)selector toTarget:(id)target withObject:(id)argument {
-    NSLog(@"void)detachNewThreadSelector:(SEL)selector toTarget:(id)%@ withObject:(id)%@", target, argument);
-    ZKOrig(void, selector, target, argument);
-    /*static int calls = 0;
-    if (calls < 3) {
-        ZKOrig(void, selector, target, argument);
-        calls++;
-    }*/
+//Normally, notifications will only appear if the CodeRunner window is not in focus.
+//We want notifications to always appear, because the running tab may not be in focus even if the window is.
+- (void)deliverUserNotificationWithTitle:(id)titleText {
+    [self displayNotificationViaTerminalNotifier: titleText];
 }
 
-- (void)start {
-    NSLog(@"start");
-    ZKOrig(void);
-}
-
-- (void)main {
-    NSLog(@"main");
-    ZKOrig(void);
+- (void)displayNotificationViaTerminalNotifier: (NSString *)titleText {
+    //The normal way to make notifications appear when the app window is in focus
+    //is to override [userNotificationCenter:shouldPresentNotification].
+    //Unfortunately, we can't do that via swizzling, so we'll use Terminal Notifier.
+    
+    NSString *terminalNotifier = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"terminal-notifier/Contents/MacOS/terminal-notifier"];
+    NSString *command = [NSString stringWithFormat:@"%@ -sender \"%@\" -title \"%@\" -message \"%@\"", terminalNotifier, [[NSBundle mainBundle] bundleIdentifier], titleText, [self fullDisplayName]];
+    NSTask *task = [[NSTask alloc] init];
+    task.environment = @{};
+    [task setLaunchPath:@"/bin/sh"];
+    [task setArguments:@[@"-c", command]];
+    
+    NSPipe *pipe = [[NSPipe alloc] init];
+    [pipe fileHandleForReading];
+    [task setStandardOutput:pipe];
+    [task launch];
 }
 
 @end
@@ -447,11 +320,8 @@ BOOL shouldPreventNSAttributeDictionaryRelease; //Warning: global variable!
     ZKSwizzle(myNSAttributeDictionary, NSAttributeDictionary);
     ZKSwizzle(myNSUserDefaults, NSUserDefaults);
     ZKSwizzle(myConsoleTextView, ConsoleTextView);
-    
-    ZKSwizzle(myProcess, Process);
     ZKSwizzle(myProcessManager, ProcessManager);
-    ZKSwizzle(myRunner, Runner);
-    ZKSwizzle(myNSThread, NSThread);
+    ZKSwizzle(myDocument, Document);
 }
 
 @end
